f513e3bcd9b1f423043db95be45cb43c9a10b181
Lucas Vinicius <lucas.joanico13@gmail.com>
2024-10-20 20:48:03 -0300

    aula 12 js

---
+++ aula12/ex012.js
@@ -0,0 +1,14 @@
+var agora = new Date()
+var hora = agora.getHours()
+var minutos = agora.getMinutes()
+var segundos = agora.getSeconds()
+
+console.log(`Agora são exatamente ${hora}h e ${minutos} minutos`)
+
+if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora < 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+} 
